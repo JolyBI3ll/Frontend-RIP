806965c9fe568bca7b152b027322ec382cca52f9
Mamou Asman <asman2003pda45@gmail.com>
2023-12-24 18:49:03 +0300

    small changes image

--- RIP_front-frontend_1/src/components/ProductCard/ProductCard.tsx
+++ RIP_front-frontend_1/src/components/ProductCard/ProductCard.tsx
@@ -11,7 +11,7 @@ interface Props {
 // "data:image/jpeg;base64,"+
 const ProductCard: FC<Props> = ({ pk, title, image }) => (
     <Card className="card">
-        <div className="cardImageWrap"><a href={"products/" + pk.toString()}><Card.Img className="cardImage" src={`data:image/jpeg;base64,${image}`} height={100} width={100} /></a></div>
+        <div className="cardImageWrap"><a href={"products/" + pk.toString()}><Card.Img className="cardImage" src={`${image}`} height={100} width={100} /></a></div>
         <div className="cardTitleWrap"><a href={"products/" + pk.toString()}><Card.Title className="cardTitle">{title}</Card.Title></a></div>
         <h4 className="cardStatusGreen">подробнее</h4>
     </Card>
